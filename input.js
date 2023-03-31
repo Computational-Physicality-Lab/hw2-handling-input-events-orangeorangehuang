f6dcc10c9c9cbd825a71e4f152d79f27a42301eb
orangeorangehuang <orange1052110128@gmail.com>
2023-03-31 21:48:25 +0800

    Edit

--- input.js
+++ input.js
@@ -282,6 +282,7 @@ workspace.addEventListener(
       touchState = 'focused';
     } 
     else if (touchState === 'focused') {
+      document.getElementById('debug').innerText = e.touches.length;
       if (e.touches.length >= 1) {
         // Resize
         document.getElementById('debug').innerText = "Resizing";
