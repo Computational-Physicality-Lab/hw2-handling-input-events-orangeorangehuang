2b5bfb329f87a199425849bcd0551ebf789da1b1
orangeorangehuang <orange1052110128@gmail.com>
2023-04-01 14:39:27 +0800

    Edit

--- input.js
+++ input.js
@@ -246,16 +246,15 @@ targets.forEach((target) => {
 
       // touch
       console.log('touchend', touchState);
-      // if (touchState == 'abortDragging') {
-      //   if (touchFocusTarget !== null) {
-      //     touchState = 'focused';
-      //   }
-      //   else {
-      //     touchState = 'pending';
-      //   }
-      // }
-      // else 
-      if (touchState == 'pending' || touchState == 'focused') {
+      if (touchState == 'abortDragging') {
+        if (touchFocusTarget !== null) {
+          touchState = 'focused';
+        }
+        else {
+          touchState = 'pending';
+        }
+      }
+      else if (touchState == 'pending' || touchState == 'focused') {
         document.getElementById('debug').innerText = 'pending/ focused';
         clearAllSelectBoxes();
         touchFocusTarget = touchTempTarget;
